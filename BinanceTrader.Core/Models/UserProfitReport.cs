c1edb8a73706b6ff8a06fcfbcf334accceb97be8
Karen Martikyan <karen.martikyan@beaxy.com>
2019-10-12 19:10:28 +0400

    Newline changes

--- BinanceTrader.Core/Models/UserProfitReport.cs
+++ BinanceTrader.Core/Models/UserProfitReport.cs
@@ -2,8 +2,8 @@
 
 namespace BinanceTrader.Core.Models
 {
-    public class UserProfitReport
-    {
-        public string UserId { get; set; }        public bool IsFullReport { get; set; }        public int TotalTradesCount { get; set; }        public decimal StartBalance { get; set; }        public decimal EndBalance { get; set; }        public double SuccessFailureRatio { get; set; }        public double AverageProfitPerHour { get; set; }        public double AverageTradesPerHour { get; set; }        public TimeSpan AverageTradeThreshold { get; set; }        public TimeSpan MinimalTradeThreshold { get; set; }
+    public class UserProfitReport    {        public string UserId { get; set; }        public bool IsFullReport { get; set; }        public int TotalTradesCount { get; set; }        public decimal StartBalance { get; set; }        public decimal EndBalance { get; set; }        public double SuccessFailureRatio { get; set; }        public double AverageProfitPerHour { get; set; }        public double AverageTradesPerHour { get; set; }
+        public TimeSpan AverageTradeThreshold { get; set; }
+        public TimeSpan MinimalTradeThreshold { get; set; }
     }
 }
